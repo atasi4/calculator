45d623add593be8b5afc28df9f4e4d43096ae2e9
Atasi <atasibhoumik@gmail.com>
2020-09-13 16:03:48 +0530

    Updates

---
+++ precache-manifest.c15c20b094b5e4a66678696aa4c238fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a7fe2ad31912b9d8c198a63fd88d82d",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "8065a6d28f7e5d387b7c",
+    "url": "/calculator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ecada51e1a3334c42b99",
+    "url": "/calculator/static/js/2.e6f076d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.e6f076d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8065a6d28f7e5d387b7c",
+    "url": "/calculator/static/js/main.7a8dcc8f.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
